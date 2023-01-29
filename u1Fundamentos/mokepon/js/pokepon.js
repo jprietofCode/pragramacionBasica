3700685c5b003ff2cac4fa4a92f8dc7e36c801ae
jprietofCode <japrieto69@misena.edu.co>
2023-01-29 08:17:29 -0500

    se agrega la funciones para seleccionar ataque de la mascota que seleccione el jugador y la maquina

--- u1Fundamentos/mokepon/js/pokepon.js
+++ u1Fundamentos/mokepon/js/pokepon.js
@@ -1,7 +1,53 @@
+let ataqueJugador;
+let ataqueEnemigo;
+function btnAtaque(id_Btn, ataque){
+    return document.getElementById(id_Btn).addEventListener('click', ataqueMascota(ataque));
+}
+
 function iniciarJuego(){
     let botonMascotaJugador = document.getElementById('btn-mascota');
-    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
+
+    let boton = document.querySelector('#select-ataque');
+    boton.addEventListener('click', (e)=> {
+        console.log(e.target.id);
+        // verifica el id del elemento clickeado
+        if(e.target.id === 'btn-fuego'){
+            btnAtaque('btn-fuego', 'fuego');
+        }else if(e.target.id === 'btn-agua'){
+            btnAtaque('btn-agua', 'agua');
+        }else if(e.target.id === 'btn-tierra'){
+            btnAtaque('btn-tierra', 'tierra');
+        }
+    })
+    /*btnAtaque('btn-fuego', 'fuego');
+    btnAtaque('btn-agua', 'agua');
+    btnAtaque('btn-tierra', 'tierra');*/
+
 }
+
+function ataqueMascota(ataque){
+    ataqueJugador = ataque;
+    alert(ataqueJugador); 
+    ataqueRival();
+}
+
+function ataqueRival(){
+    let ataqueRivalRandom = aleatorio(1,3);
+
+    if(ataqueRivalRandom === 1){
+        ataqueEnemigo = 'fuego';
+    } else if(ataqueRivalRandom === 2){
+        ataqueEnemigo = 'agua';
+    }else{
+        ataqueEnemigo = 'tierra';
+    }
+}
+
+function crearMensaje(){
+    
+}
+
 function verificarMascota(mascota){
     return document.getElementById(mascota).checked;
 }
@@ -9,6 +55,7 @@ function verificarMascota(mascota){
 function mascotaSelect(player, mascota){
     return document.getElementById(player).innerHTML = mascota;
 }
+
 function seleccionarMascotaJugador(){
     if(verificarMascota("hipodoge")){
         mascotaSelect('mascota-jugador', 'hipodoge');
@@ -38,5 +85,6 @@ function selectMascotaEnemigo(){
 function aleatorio(min, max){
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
 //end load page html
 window.addEventListener('load', iniciarJuego);
